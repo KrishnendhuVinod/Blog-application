bc1dbccca9f3c757e811fa1e318a454e0003e382
krishnendhu <krishnendhuvinodkumar393@gmail.com>
2025-06-25 11:17:11 +0530

    Updated

--- blog_api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-class PostSerializer(serializers.ModelSerializer) :
-  author=serializers.StringRelatedField(read_only=True)
-
-  class Meta:
-    model = Post
-    fields=['id','title','summary','content','author','created_at']
